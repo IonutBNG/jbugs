d3372a8ba9373a9a905c5e0cab0ad5be83ddbebd
robuvlad <robuvlad1998@gmail.com>
2019-05-21 18:49:58 +0300

    bd scripts

---
+++ jbugs-persistence/src/main/java/scripts_bd/dropTables.sql
@@ -0,0 +1,6 @@
+drop table bugs;
+drop table roles_permissions;
+drop table permissions;
+drop table users_roles;
+drop table roles;
+drop table users;
---
+++ jbugs-persistence/src/main/java/scripts_bd/initializeDB.sql
@@ -0,0 +1,66 @@
+
+create table users
+(
+	id bigint auto_increment primary key,
+	first_name varchar(255) not null,
+	last_name varchar(255) not null,
+	mobile_number varchar(255) not null,
+	email varchar(255) not null,
+	username varchar(255) not null,
+	password varchar(255) not null,
+	counter int null
+);
+
+create table roles
+(
+	id bigint auto_increment primary key,
+	type varchar(255) not null
+);
+
+create table users_roles
+(
+	user_id bigint not null,
+	role_id bigint not null,
+	primary key (user_id, role_id),
+	constraint FK_users_roles_roles_id
+		foreign key (role_id) references roles (id),
+	constraint FK_users_roles_users_id
+		foreign key (user_id) references users (id)
+);
+
+create table permissions
+(
+	id bigint auto_increment primary key,
+	description varchar(255) not null,
+	type varchar(255) not null
+);
+
+create table roles_permissions
+(
+	role_id bigint not null,
+	permission_id bigint not null,
+	primary key (role_id, permission_id),
+	constraint FK_permissions_roles_permissions_id
+		foreign key (permission_id) references permissions (id),
+	constraint FK_permissions_roles_roles_id
+		foreign key (role_id) references roles (id)
+);
+
+create table bugs
+(
+    id bigint auto_increment primary key,
+    title varchar(255) not null,
+    description varchar(255) not null,
+    version varchar(255) not null,
+    target_date datetime not null,
+    status varchar(255) not null,
+    fixed_version varchar(255) not null,
+    severity varchar(255) CHECK(severity="critical" OR severity="high" OR
+            severity="medium" OR severity="low"),
+    createdByUser bigint not null,
+    assignedTo bigint not null,
+    constraint FK_bugs_users_created_id
+		foreign key (createdByUser) references users (id),
+	constraint FK_bugs_users_assigned_id
+		foreign key (assignedTo) references users (id)
+);
